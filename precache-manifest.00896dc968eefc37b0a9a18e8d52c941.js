ffd9815dc527372642222b495e5c9d20dc03c561
Callum <38749032+howzitcal@users.noreply.github.com>
2020-10-14 14:59:58 +0200

    Initial commit

---
+++ precache-manifest.00896dc968eefc37b0a9a18e8d52c941.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ae9da9ba274a09a6ebae8f7f1f143a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15a74a7088a8bef59004",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "08a82d1a185dde95fcb9",
+    "url": "/static/js/2.dc7d7b42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dc7d7b42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15a74a7088a8bef59004",
+    "url": "/static/js/main.cfc6647b.chunk.js"
+  },
+  {
+    "revision": "1d28864e2b53a7951e1f",
+    "url": "/static/js/runtime-main.67041c06.js"
+  }
+]);
